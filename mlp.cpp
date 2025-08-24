a61bcf127d56a38a50191c09ca8c15e764feafcf
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-08-23 23:20:27 -0400

    mlp(feat): Add a simple sketch to setup the model

--- mlp.cpp
+++ mlp.cpp
@@ -2,7 +2,11 @@
 // a multi-layer perceptron in C++ completely from scratch.
 // the code will lean into C heavily while leveraging C++ utils to reduce complexity.
 // no classes or templates are allowed. minimal usage of vectors, maps, etc. is valid.
+// auto is banned from usage! it is an anti-pattern to hide data types!
+#include <ctime>
+#include <cstdlib>
 #include <cstdio>
+#include <vector>
 
 // Model dimensions
 struct MLPParams {
@@ -13,22 +17,41 @@ struct MLPParams {
 };
 
 // Model layers
-struct MLPLayers {
-    float* W = nullptr;  // flat 2d matrix
-    float* b = nullptr;  // 1d vector
+struct MLPLayer {
+    float* W = nullptr;  // n_out x n_in
+    float* b = nullptr;  // n_out
 };
 
 // Model
 struct MLP {
+    // setup model layers
+    std::vector<struct MLPLayer> layers;
+
+    // input/output tensors
     float* x = nullptr;  // 1d input vector
     float* y = nullptr;  // 1d output vector
 
-    struct MLPLayers* layers;
-
-    struct MLPParams {};
+    // setup model params
+    struct MLPParams params{};
 };
 
 int main(void) {
-    printf("Hello, world!\n");
+    srand(time(NULL));
+
+    // initialize the model
+    MLP mlp{};
+    // initialize the model layers
+    mlp.layers.resize(mlp.params.n_layers);
+
+    // initialize the input vector
+    std::vector<float> x(mlp.params.n_in);
+    mlp.x = x.data();
+    for (size_t i = 0; i < x.size(); i++) {
+        mlp.x[i] = (float) rand() / (float) RAND_MAX;  // normalized distribution
+    }
+
+    for (int i = 0; i < mlp.params.n_in; i++) {
+        printf("mlp.x[%d] = %.6f\n", i, (double) mlp.x[i]);
+    }
     return 0;
 }
