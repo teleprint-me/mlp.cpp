9795a06556d8c11a29d5585b4115cb64b8bb892a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-09-08 03:41:17 -0400

    data(feat): Enable shuffling and selecting up to n samples per class

--- mnist/mnist_data.cpp
+++ mnist/mnist_data.cpp
@@ -22,6 +22,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#include "xorshift.h"
 #include "path.h"
 
 // MNIST image length
@@ -37,7 +38,22 @@ struct MNISTSample {
     std::vector<float> pixels{}; /**< Flattened pixel data (grayscale values). */
 };
 
+// shuffle an array of indices [0, n)
+void shuffle_indices(size_t* values, size_t n, uint32_t (*rng)(void)) {
+    for (size_t i = n - 1; i > 0; i--) {
+        // random index [0, i]
+        size_t j = rng() % (i + 1);
+        // swap indices i with j
+        size_t temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+    }
+}
+
 int main(void) {
+    uint64_t seed = 1337;
+    xorshift_init(seed);
+
     char dataset_path[] = "data/mnist/training";
 
     std::vector<MNISTSample> samples{}; /**< Array of MNIST samples. */
@@ -48,6 +64,7 @@ int main(void) {
         return 1;
     }
 
+    size_t n_samples = 10;  // select up to 10 samples
     for (size_t i = 0; i < dirs_count; i++) {
         // extract label from dirname
         char* label_str = path_basename(dirs[i]);
@@ -57,12 +74,23 @@ int main(void) {
         size_t files_count;
         char** files = path_list_files(dirs[i], &files_count);
 
-        for (size_t j = 0; j < files_count; j++) {
+        // Build a list of indices
+        std::vector<size_t> indices(files_count);
+        for (size_t k = 0; k < files_count; ++k) {
+            indices[k] = k;
+        }
+
+        shuffle_indices(indices.data(), indices.size(), xorshift_gen_int32);
+        size_t max_samples = std::min(n_samples, files_count);
+        for (size_t j = 0; j < max_samples; j++) {
+            // select images at random and bind them to the file count
+            uint32_t idx = indices[j];
+
             // load image and force grayscale
             int width, height, channels;
-            uint8_t* data = stbi_load(files[j], &width, &height, &channels, 1);
+            uint8_t* data = stbi_load(files[idx], &width, &height, &channels, 1);
             if (!data || IMAGE_LEN != width || IMAGE_LEN != height) {
-                printf("[skip] %s (bad image)\n", files[j]);
+                printf("[skip] %s (bad image)\n", files[idx]);
                 if (data) {
                     stbi_image_free(data);
                 }
